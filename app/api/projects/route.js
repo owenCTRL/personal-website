54897bd93a46a04458375069949656cb85f4e90e
owen <owen.ottr@gmail.com>
2025-07-08 15:17:31 -0400

    projects page

--- app/api/projects/route.js
+++ app/api/projects/route.js
@@ -4,28 +4,28 @@ export async function GET(request) {
       name: "OTTR - Over The Top Returns",
       description:
         "Private Equity Group at the intersection of our most important industries and technologies. Whether it be for Financial Arbitrage, Political Consulting, Next-Gen AI Integrations, or Quantitative Research and Modeling, OTTR provides 'Over The Top Returns', regardless of the industry.",
-      stack: ["Typescript", "NextJS", "TailwindCSS", "Python", "Solidity", "Rust"],
+      stack: ["Software", "Politics", "Finance", "100x Strategy Design"],
       link: "https://x.com/ottrCo",
     },
     {
       name: "Sandbox Research - Test. Tweak. Win.",
       description:
         "Strategy, Research and Modeling consulting firm. Sandbox Research is a think tank that provides services to help you achieve your goals, whatever they may be.",
-      stack: ["Typescript", "NextJS", "Linear Algebra", "Python"],
+      stack: ["Growth Strategy", "Predictive Modeling", "Copy/Writing", "and more..."],
       link: "https://sandbox-research.vercel.app/",
     },
     {
       name: "Clickroot - Mission Critical, By Design",
       description:
-        "Defense-oriented software solutions and data protection. Clickroot is a software development company focused on building secure, reliable, and mission-critical applications for the <b>general public</b>, not the defense sector.",
-      stack: ["Typescript", "NextJS"],
+        "Defense-oriented software solutions. Clickroot is a software development company focused on building secure, reliable, and mission-critical applications for the <b>general public</b>, not the defense sector.",
+      stack: ["TruthGuard"],
       link: "https://X.com/Clickroot",
     },
     {
       name: "TruthGuard (Early Development) - Decentralized News Verification Platform",
       description:
-        "By Clickroot: News verification protocol meant to introduce abstracted information while removing biases and misinformation from the news ecosystem.",
-      stack: ["Typescript", "NextJS", "TailwindCSS"],
+        "News verification protocol meant to introduce abstracted information while removing biases and misinformation from the news ecosystem.",
+      stack: ["Typescript", "NextJS", "TailwindCSS", "and more..."],
       link: "https://truthguard-clickroot.vercel.app",
     },
   ];
